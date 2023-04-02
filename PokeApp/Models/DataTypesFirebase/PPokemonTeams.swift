f4230075172a8feb7d98d0b0af1b38d003b033bf
Carlos Paredes <carlosparedes@Carloss-MacBook-Pro.local>
2023-04-02 11:54:13 -0600

    [UPDATE] PokeApp TeamsView

---
+++ PokeApp/Models/DataTypesFirebase/PPokemonTeams.swift
@@ -0,0 +1,8 @@
+//
+//  PPokemonTeams.swift
+//  PokeApp
+//
+//  Created by Carlos Paredes on 2/8/23.
+//
+
+import Foundation
---
+++ PokeApp/ViewModels/Team/PPokemonTeamListViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PPokemonTeamListView.swift
+//  PokeApp
+//
+//  Created by Carlos Paredes on 2/7/23.
+//
+
+import Foundation
